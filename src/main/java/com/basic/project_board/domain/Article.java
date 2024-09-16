df181332900db61c3d12d5475e3b6f3cda4484b3
otteu <otteu1m@gmail.com>
2024-09-16 18:59:08 +0900

    #1 - 도메인 클래스 생성(게시글, 댓글)

---
+++ src/main/java/com/basic/project_board/domain/Article.java
@@ -0,0 +1,14 @@
+package com.basic.project_board.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String title;               // 제목
+    private String hashtag;             // 해시태그
+
+    private LocalDateTime createAt;     // 생성일자
+    private String createdBy;           // 생성자
+    private LocalDateTime modifiedAt;   // 수정일자
+    private String modifiedBy;          // 수정자
+}
