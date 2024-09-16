77b3d7af67021de797fc46dd9bff31050b75bfb8
otteu <otteu1m@gmail.com>
2024-09-16 22:59:05 +0900

    #10 - Test Code 프러파일 추가

--- src/test/java/com/basic/project_board/repository/JpaRepositoryTest.java
+++ src/test/java/com/basic/project_board/repository/JpaRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 
+// @ActiveProfiles("testdb")
 @DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
 @DataJpaTest
@@ -96,5 +98,5 @@ class JpaRepositoryTest {
                 .isEqualTo(saveCount - 1);
     }
 
-    
+
 }
